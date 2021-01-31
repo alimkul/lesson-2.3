dc6cc298aa560f999b2d99ab78c8ec423cf487b2
Archie <alimkul.shynggys@gmail.com>
2021-02-01 00:41:30 +0600

    Решения 3 задания

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
 fun main(){
+    var amont :Double =10001.00
+    var check : Boolean=true
+    if(amont>1000 && amont<10001)
+    amont-=100
+    else(amont>10000)
+        amont-=amont*0.05/100;
 
+    if(check)
+        amont-=amont*0.01/100
+    print("Amount: "+ amont)
 }
