b21a8659910f86d9ad4af6296382ecfb0d487268
Archie <alimkul.shynggys@gmail.com>
2021-02-03 22:16:45 +0600

    Update 2.1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,16 +4,20 @@ fun main(){
     var amount :Double =100000.00
     var check : Boolean=true
 
-   result("Mastercard",previousAmount, amount )
+   result("Vk Pay",previousAmount, amount )
 }
 fun result(typeCard: String="Vk Pay", previousAmount: Int, amount: Double){
+    var nalog=0.0
     when(typeCard){
         "Mastercard", "Maestro" -> if(! (amount >= 300 && amount <= 75000) ) {
-           print ("Amount: "+(amount -(amount*0.6 /100 +20)) + "налог "+(amount*0.6 /100 +20))
+            nalog=amount*0.6 /100 +20
 
         }
-        "Visa" , "Мир" -> if( amount * 0.75/100 <=35 ) println("Amount: " + (amount - 35) +" налог 35 руб")
-                    else print("Amount: "+(amount -amount * 0.75/100 )+" налог " +amount * 0.75/100)
-        "Vk Pay" -> print("Amount: "+amount+" налог 0.0" )
+        "Visa" , "Мир" -> if( amount * 0.75/100 <=35 )
+                             nalog=35.0
+                    else nalog=amount * 0.75/100
+        "Vk Pay" -> nalog=0.0
     }
+    print ("Amount: "+(amount -nalog) + " налог "+nalog)
+
 }
